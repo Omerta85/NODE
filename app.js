4653caea95a38c0594583596820305d84f5c9de2
Omerta85 <666Omerta@gmail.com>
2022-12-01 19:00:53 +0200

    update lesson4

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose =  require('mongoose');
+require('dotenv').config()
+
+const {userRouter, carRouter} = require('./router');
+const configs = require('./config/config');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/users', userRouter);
+app.use('/cars', carRouter);
+
+app.get('/', (req, res) => {
+    res.json('WELOCME')
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message || 'Unknown error',
+        status: err.status || 500
+    });
+});
+
+app.listen(configs.PORT, async () => {
+    console.log(`Server listen ${configs.PORT}`);
+    await mongoose.connect(configs.MONGO_URL);
+
+});
